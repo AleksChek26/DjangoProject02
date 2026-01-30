67618bf251f966d1a8f1a04a3d7813b1c5b4447e
AleksChek26 <check1984@gmail.com>
2026-01-30 14:30:06 +0500

    add_structure

---
+++ materials/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+class Course(models.Model):
+    title = models.CharField(max_length=255, verbose_name="Название курса")
+    preview = models.ImageField(upload_to='courses/', verbose_name="Превью", null=True, blank=True)
+    description = models.TextField(verbose_name="Описание")
+
+    class Meta:
+        verbose_name = "Курс"
+        verbose_name_plural = "Курсы"
+
+    def __str__(self):
+        return self.title
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='lessons', verbose_name="Курс")
+    title = models.CharField(max_length=255, verbose_name="Название урока")
+    description = models.TextField(verbose_name="Описание")
+    preview = models.ImageField(upload_to='lessons/', verbose_name="Превью", null=True, blank=True)
+    video_url = models.URLField(verbose_name="Ссылка на видео", null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Урок"
+        verbose_name_plural = "Уроки"
+
+
+    def __str__(self):
+        return self.title
