da44ae411a2fe5aefe03b554f41b11be49803352
AleksChek26 <check1984@gmail.com>
2026-02-11 21:13:32 +0500

    add_stripe

--- users/urls.py
+++ users/urls.py
@@ -1,16 +1,22 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.permissions import AllowAny
+from rest_framework.routers import DefaultRouter
 
 from users.apps import UsersConfig
-from users.views import PaymentListAPIView, UserCreateAPIView
+from users.views import PaymentListAPIView, UserCreateAPIView, PaymentViewSet
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
+router = DefaultRouter()
+router.register(r'payments', PaymentViewSet, basename='payments')
+
+
 app_name = UsersConfig.name
 
 urlpatterns = [
+    path('', include(router.urls)),
     path("payments/", PaymentListAPIView.as_view(), name="payment_list"),
     path("register/", UserCreateAPIView.as_view(), name="register"),
     path('token/', TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name='token_obtain_pair'),
